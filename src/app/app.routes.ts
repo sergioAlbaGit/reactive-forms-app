53ca62411e40593f76661f1b6db58ea583d3b45d
sergioAlbaGit <sergioalbatorres@hotmail.com>
2025-05-02 19:20:24 +0200

    feat: add routes and templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () =>
+      import('./reactive/reactive.routes').then((m) => m.reactiveRoutes)
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.routes')  // funciona sin then porque es un default export
+    },
+  {
+    path: 'country',
+    loadChildren: () =>
+      import('./country/country.routes') .then((m) => m.countryRoutes)
+    },
+  {
+    path: '**',
+    redirectTo: 'reactive' // se redirige a la ruta por defecto de reactive, que es la de basic
+    },
+];
