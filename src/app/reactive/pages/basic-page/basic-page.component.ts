4d3553290e053d8a14607c194b072a7c12d19c7b
sergioAlbaGit <sergioalbatorres@hotmail.com>
2025-05-05 17:47:02 +0200

    feat: add reactive forms

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,26 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+
+  private fb = inject(FormBuilder)
+
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage:[0, [Validators.required, Validators.min(0)]],
+  })
+
+ myForm2 = new FormGroup({
+   name: new FormControl(''),
+   price: new FormControl(0),
+   inStorage: new FormControl(0),
+ })
+
+}
