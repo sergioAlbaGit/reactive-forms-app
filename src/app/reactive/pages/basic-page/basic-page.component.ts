ff24251111a17b6dff581cc303f6a5895ccc2f40
sergioAlbaGit <sergioalbatorres@hotmail.com>
2025-05-08 18:12:47 +0200

    feat: add reactive forms and show errors

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -11,7 +11,7 @@ export class BasicPageComponent {
 
   private fb = inject(FormBuilder)
 
-  myForm = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage:[0, [Validators.required, Validators.min(0)]],
@@ -22,5 +22,46 @@ export class BasicPageComponent {
    price: new FormControl(0),
    inStorage: new FormControl(0),
  })
+
+ isValidField(fieldName: string): boolean | null{
+  return (this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched)
+ }
+
+ getFieldError(fieldName: string): string | null {
+  if ( !this.myForm.controls[fieldName] ) return null
+
+  const errors = this.myForm.controls[fieldName].errors ?? {};
+
+  for (const key of Object.keys(errors)){
+    switch (key){
+      case 'required':
+        return 'Este campo es requerido'
+      case 'minlength':
+        return `Mínimo de ${errors['minlength'].requiredLength}`
+      case 'min':
+        return `Valor mínimo de ${errors['min'].min}`
+    }
+  }
+  return null
+ }
+
+
+onSave(){
+ if(this.myForm.invalid){
+
+   this.myForm.markAllAsTouched();
+   return;
+  }
+  console.log(this.myForm.value);
+
+
+  this.myForm.reset({
+    price: 0,
+    inStorage: 0
+  })
+
+
+  }
+
 
 }
